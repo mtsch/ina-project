823feed4da0d3ffe634b017d749550463a1f6e5b
mtsch <matijacufar@gmail.com>
2017-05-07 14:19:44 +0200

    Added metadata csv

--- src/mat2net.jl
+++ src/mat2net.jl
@@ -1,4 +1,5 @@
 using COBRA
+using JSON
 using LightGraphs
 
 function mat2net(filename="../models/iCHOv1-model/iCHOv1.mat", name="iCHOv1")
@@ -27,4 +28,27 @@ function mat2net(filename="../models/iCHOv1-model/iCHOv1.mat", name="iCHOv1")
     end
 
     g, rxns
+end
+
+function create_csv(filename = "../models/iCHOv1-model/iCHOv1-meta.csv";
+                    jsonfile = "../models/iCHOv1-model/iCHOv1.json",
+                    matfile  = "../models/iCHOv1-model/iCHOv1.mat",
+                    name     = "iCHOv1")
+
+    metadata = JSON.parse(readstring(jsonfile))
+    _, rxns  = mat2net(matfile, name)
+
+    output = Array{String, 2}(length(rxns) + 1, 4)
+    output[1, :] = ["node", "name_1", "name_2", "subsystem"]
+
+    for (i, r) in enumerate(metadata["reactions"])
+        println("$(r["name"]) === $(rxns[i])")
+        output[i+1, :] .= string.([i,
+                                   rxns[i],
+                                   r["name"],
+                                   r["subsystem"]])
+    end
+
+    writecsv(filename, output)
+    output
 end
